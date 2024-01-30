bf785b7b767280f9a0a0f626d14f661ffd514bf8
Ramon Miguel <ramonmiguel3394@gmail.com>
2024-01-29 21:30:20 -0300

    alterando limite para 100

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();
@@ -33,7 +33,7 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
+    return parseInt(Math.random() * 100 + 1);
 }
 
 function limparCampo() {
